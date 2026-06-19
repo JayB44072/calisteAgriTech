2d5a204b78280b61df88aab93e0a9206b763e906
jayb44072 <jayb44072@gmail.com>
2026-06-19 18:48:26 +0200

    version bolt

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,5 +6,5 @@ export const ROLES = {
   fournisseur: { label: 'Fournisseur de services', labelEn: 'Service Provider', color: 'text-blue-600', bg: 'bg-blue-50 dark:bg-blue-900/20' },
 } as const;
 
-export const CULTURES = ['Tomates', 'Piment', 'Maïs', 'Manioc', 'Haricot', 'Arachide', 'Cacao', 'Café', 'Plantain', 'Igname', 'Macabo', 'Légumes'];
+export const CULTURES = ['Tomates', 'Piment', 'Maïs', 'Manioc', 'Haricot', 'Arachide', 'Cacao', 'Café', 'Plantain', 'Igname', 'Macabo', 'Légumes','Orange'];
 export const ZONES = ['Centre', 'Littoral', 'Ouest', 'Nord-Ouest', 'Sud-Ouest', 'Est', 'Sud', 'Nord', 'Extrême-Nord', 'Adamaoua'];
