f9882f29af2ad41aeb8c2d8730d96cd33b84ea7c
jayb44072 <jayb44072@gmail.com>
2026-06-20 14:20:07 +0200

    selecteur de compte

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -41,6 +41,7 @@ export function useAuth() {
       provider: 'google',
       options: {
         redirectTo: window.location.origin,
+        queryParams: { prompt: 'select_account' },
       },
     });
     if (error) throw error;
